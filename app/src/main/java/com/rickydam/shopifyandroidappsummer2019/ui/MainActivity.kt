22e90af89e587440435dad447fd5500e97ee7f5e
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-13 01:55:49 -0500

    Move MainActivity into the ui package

--- app/src/main/java/com/rickydam/shopifyandroidappsummer2019/ui/MainActivity.kt
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rickydam.shopifyandroidappsummer2019
+package com.rickydam.shopifyandroidappsummer2019.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.rickydam.shopifyandroidappsummer2019.R
 
 class MainActivity : AppCompatActivity() {
 
