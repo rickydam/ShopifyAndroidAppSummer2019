7fa49e2176ac48626c35dcca4c5058e37ccee55d
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-17 12:10:11 -0500

    Create the CustomCollectionsFragment with its ViewModel

---
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/CustomCollectionsViewModel.kt
@@ -0,0 +1,7 @@
+package com.rickydam.shopifyandroidappsummer2019.viewmodel
+
+import android.arch.lifecycle.ViewModel;
+
+class CustomCollectionsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
