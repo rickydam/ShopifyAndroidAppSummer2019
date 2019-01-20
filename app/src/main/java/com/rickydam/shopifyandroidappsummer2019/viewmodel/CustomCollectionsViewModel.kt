0fc7a18ac1cc647a4b87a0f4907640e5c07746d0
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-20 18:53:12 -0500

    Remove unused import in CustomCollectionsVM

--- app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/CustomCollectionsViewModel.kt
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/CustomCollectionsViewModel.kt
@@ -1,6 +1,5 @@
 package com.rickydam.shopifyandroidappsummer2019.viewmodel
 
-import android.arch.lifecycle.ViewModel
 import com.rickydam.shopifyandroidappsummer2019.data.remote.ShopifyAPI
 import javax.inject.Inject
 
