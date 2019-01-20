cdf2c5e2e883146417280841c5f39e444745a1ac
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-20 18:54:08 -0500

    Add methods to retrieve the CustomCollections in the VM

--- app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/CustomCollectionsViewModel.kt
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/CustomCollectionsViewModel.kt
@@ -1,9 +1,43 @@
 package com.rickydam.shopifyandroidappsummer2019.viewmodel
 
 import com.rickydam.shopifyandroidappsummer2019.data.remote.ShopifyAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class CustomCollectionsViewModel : BaseViewModel() {
     @Inject
     lateinit var shopifyAPI: ShopifyAPI
+
+    private lateinit var subscription: Disposable
+
+    init {
+        loadCustomCollections()
+    }
+
+    private fun loadCustomCollections() {
+        subscription = shopifyAPI.getCustomCollections()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrieveCustomCollectionsStart() }
+            .doOnTerminate { onRetrieveCustomCollectionsFinish() }
+            .subscribe(
+                { onRetrieveCustomCollectionsSuccess() },
+                { onRetrieveCustomCollectionsError() }
+            )
+    }
+
+    private fun onRetrieveCustomCollectionsStart() {}
+
+    private fun onRetrieveCustomCollectionsFinish() {}
+
+    private fun onRetrieveCustomCollectionsSuccess() {}
+
+    private fun onRetrieveCustomCollectionsError() {}
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
