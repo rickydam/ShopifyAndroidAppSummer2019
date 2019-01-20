6f85590913647608761a29a106b0ffe5db5ded49
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-20 13:10:31 -0500

    Create the ShopifyAPI interface for Retrofit

---
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/data/remote/ShopifyAPI.kt
@@ -0,0 +1,16 @@
+package com.rickydam.shopifyandroidappsummer2019.data.remote
+
+import com.rickydam.shopifyandroidappsummer2019.model.CustomCollection
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+/**
+ * The interface that provides Retrofit HTTP requests
+ */
+interface ShopifyAPIService {
+    /**
+     * Get the list of CustomCollections from the Shopify API
+     */
+    @GET("/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
+    fun apiGetCustomCollections(): Observable<List<CustomCollection>>
+}
