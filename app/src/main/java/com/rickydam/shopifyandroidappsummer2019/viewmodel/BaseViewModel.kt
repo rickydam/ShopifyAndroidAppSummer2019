0ba0544e4bd5d6ced88a0dd6ad06e4e64b3f4486
Ricky Dam <RickyDamOttawa@gmail.com>
2019-01-20 18:38:37 -0500

    Use the injector in BaseViewModel

--- app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/rickydam/shopifyandroidappsummer2019/viewmodel/BaseViewModel.kt
@@ -1,6 +1,26 @@
 package com.rickydam.shopifyandroidappsummer2019.viewmodel
 
 import android.arch.lifecycle.ViewModel
+import com.rickydam.shopifyandroidappsummer2019.injection.component.DaggerViewModelInjector
+import com.rickydam.shopifyandroidappsummer2019.injection.component.ViewModelInjector
+import com.rickydam.shopifyandroidappsummer2019.injection.module.NetworkModule
 
 abstract class BaseViewModel: ViewModel() {
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+        .builder()
+        .networkModule(NetworkModule)
+        .build()
+
+    init {
+        inject()
+    }
+
+    /**
+     * Injects the required dependencies
+     */
+    private fun inject() {
+        when (this) {
+            is CustomCollectionsViewModel -> injector.inject(this)
+        }
+    }
 }
